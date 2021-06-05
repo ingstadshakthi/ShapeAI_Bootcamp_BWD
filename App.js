4361bfb0b4cd34b1ac363a39849b78ebe651a4e8
ingstadshakthi <59392423+ingstadshakthi@users.noreply.github.com>
2021-06-05 13:44:56 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Note from "./components/Note";
+function App() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+      <Note />
+      <Note />
+      <Note />
+    </div>
+  );
+}
+
+export default App;
